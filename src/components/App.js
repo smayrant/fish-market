d871d9d8e0eba31a6293bd1b8c9accc2841cc8cb
Sheldrick <mrmayrant@yahoo.com>
2018-11-01 05:11:17 -0400

    added an AddFishForm component and its corresponding functionality

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,21 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    }
+
+    addFish = fish => {
+        // retrieve existing state
+        const fishes = {...this.state.fishes};
+        // set new fish to the fishes variable with a unique key from Date.now
+        fishes[`fish${Date.now()}`] = fish; 
+        // update the state with the new fishes object
+        this.setState({
+            fishes
+        })
+    }
     render() {
         return (
             <div className="catch-of-the-day">
@@ -11,7 +26,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         )
     }

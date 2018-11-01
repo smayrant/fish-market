a1ea88cfa89c8f595d55af5414987d71f3420da3
Sheldrick <mrmayrant@yahoo.com>
2018-10-31 21:31:38 -0400

    added react router and created an event for when the user clicks on visit store

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,20 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <header className="top">
-                    <h1>Catch
-                        <span className="ofThe">
-                            <span className="of">of</span>
-                            <span className="the">the</span>
-                        </span>
-                        Day</h1>
-                    <h3 className="tagline">
-                        <span>Fresh Daily</span>
-                    </h3>
-                </header>
-            </div>
-        )
-    }
-}
+const Header = props => (
+    <div>
+        <header className="top">
+            <h1>Catch
+                <span className="ofThe">
+                    <span className="of">of</span>
+                    <span className="the">the</span>
+                </span>
+                Day</h1>
+            <h3 className="tagline">
+                <span>{props.tagline}</span>
+            </h3>
+        </header>
+    </div>
+)
+
 
 export default Header
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = (e) => {
+        // prevent the form from submitting
+        e.preventDefault()
+
+        // retrieve the text from the input field
+        const storeName = this.myInput.value.value;
+
+        // the page is changed to /store/whatever the user entered
+        this.props.history.push(`/store/${storeName}`);
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" />
+                {/* a default store name is created using the getFunName function from the helper file  */}
+                <input type="text" required placeholder="Store Name" ref={this.myInput} defaultValue={getFunName()} />
                 <button type="submit">Visit Store →</button>
             </form>
         );

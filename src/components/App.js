726eb28c320d5b5135ef3fba7be506c27ac9a2dd
Sheldrick <mrmayrant@yahoo.com>
2018-11-01 21:13:56 -0400

    added the basic functionality to add items to an order

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,69 @@
-import React from 'react';
-import Header from './Header';
-import Inventory from './Inventory';
-import Order from './Order';
-import sampleFishes from '../sample-fishes';
-import Fish from './Fish';
+import React from "react";
+import Header from "./Header";
+import Inventory from "./Inventory";
+import Order from "./Order";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
-    state = {
-        fishes: {},
-        order: {}
-    }
+  state = {
+    fishes: {},
+    order: {}
+  };
 
-    // method to add fish to the state
-    addFish = fish => {
-        // retrieve existing state
-        const fishes = {...this.state.fishes};
-        // set new fish to the fishes variable with a unique key from Date.now
-        fishes[`fish${Date.now()}`] = fish; 
-        // update the state with the new fishes object
-        this.setState({
-            fishes
-        })
-    }
+  // method to add fish to the state
+  addFish = fish => {
+    // retrieve existing state
+    const fishes = { ...this.state.fishes };
+    // set new fish to the fishes variable with a unique key from Date.now
+    fishes[`fish${Date.now()}`] = fish;
+    // update the state with the new fishes object
+    this.setState({
+      fishes
+    });
+  };
 
-    // method to display the sample set of fish to the screen
-    loadSampleFishes = () =>{
-        this.setState({
-            fishes: sampleFishes
-        })
-    }
-    render() {
-        return (
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh Seafood Market" />
-                    <ul className='fishes'>
-                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
-                    </ul>
-                </div>
-                <Order />
-                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
-            </div>
-        )
-    }
+  // method to display the sample set of fish to the screen
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes
+    });
+  };
+
+  addToOrder = key => {
+    // retrieve copy of state
+    const order = { ...this.state.order };
+    // fish is added to order or quantity is updated
+    order[key] = order[key] + 1 || 1;
+    // update the state
+    this.setState({
+      order
+    });
+  };
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
+        </div>
+        <Order />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,9 +1,14 @@
 import React from "react";
+// importing the formatPrice function from the helpers file
 import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
+  handleClick = () => {
+    this.props.addToOrder(this.props.index);
+  };
   render() {
     const { image, name, price, desc, status } = this.props.details;
+    const isAvailable = status === "available";
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -12,7 +17,10 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Cart</button>
+        {/* the button is disabled when isAvailable is false */}
+        <button disabled={!isAvailable} onClick={this.handleClick}>
+          {isAvailable ? "Add To Order" : "Out Of Stock"}
+        </button>
       </li>
     );
   }

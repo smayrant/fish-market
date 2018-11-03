4cd02cec768fea9d7e943e1b203e677c1140b5cf
Sheldrick <mrmayrant@yahoo.com>
2018-11-03 07:04:15 -0400

    added comonent validation with PropTypes

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,29 +1,40 @@
 import React from "react";
-import { getFunName } from '../helpers';
+import PropTypes from "prop-types";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    myInput = React.createRef();
+  myInput = React.createRef();
 
-    goToStore = (e) => {
-        // prevent the form from submitting
-        e.preventDefault()
+  static propTypes = {
+    history: PropTypes.object
+  };
 
-        // retrieve the text from the input field
-        const storeName = this.myInput.value.value;
+  goToStore = e => {
+    // prevent the form from submitting
+    e.preventDefault();
 
-        // the page is changed to /store/whatever the user entered
-        this.props.history.push(`/store/${storeName}`);
-    }
-    render() {
-        return (
-            <form className="store-selector" onSubmit={this.goToStore}>
-                <h2>Please Enter A Store</h2>
-                {/* a default store name is created using the getFunName function from the helper file  */}
-                <input type="text" required placeholder="Store Name" ref={this.myInput} defaultValue={getFunName()} />
-                <button type="submit">Visit Store →</button>
-            </form>
-        );
-    }
+    // retrieve the text from the input field
+    const storeName = this.myInput.value.value;
+
+    // the page is changed to /store/whatever the user entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        {/* a default store name is created using the getFunName function from the helper file  */}
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          ref={this.myInput}
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store →</button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
